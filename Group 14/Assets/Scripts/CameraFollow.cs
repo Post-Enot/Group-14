0869eaab275b2ddff7ae9e60a40423067888526b
SweetyFire <mzhelezschikov@mail.ru>
2024-04-25 19:31:42 +0600

    ThisisPlayer!

---
+++ Group 14/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _objectToFollow;
+    [SerializeField] private float _smoothSpeed = 4f;
+    [SerializeField] private Vector3 _offset;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, _objectToFollow.position + _offset, Time.deltaTime * _smoothSpeed);
+    }
+}
---
+++ Group 14/Assets/Scripts/ThisIsPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ThisIsPlayer : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private Camera _camera;
+
+    private Vector3 _moveDirection;
+
+    private void Awake()
+    {
+        _camera.transform.parent.SetParent(null);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveDirection += Vector3.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _moveDirection -= Vector3.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveDirection += Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _moveDirection -= Vector3.right;
+        }
+
+        _moveDirection = Quaternion.Euler(0f, _camera.transform.localRotation.y, 0f) * _moveDirection;
+        _moveDirection.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(_speed * Time.deltaTime * _moveDirection);
+    }
+}
